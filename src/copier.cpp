711fc4cbf4cd1cccb569285519cb905aa18ca36c
castano <castano@gmail.com>
2019-12-03 23:46:58 -0800

    Copy enums and is_union struct member.

--- src/copier.cpp
+++ src/copier.cpp
@@ -285,7 +285,21 @@ Ast_Expression *Copier::copy(Ast_Expression *expression) {
             COPY(identifier);
             copy_scope(&_new->member_scope, &old->member_scope);
             _new->member_scope.owning_struct = _new;
+            COPY_P(is_union);
             
+            return _new;
+        }
+
+        case AST_ENUM: {
+            auto old  = static_cast<Ast_Enum *>(expression);
+            auto _new = COPIER_NEW(Ast_Enum);
+            
+            COPY(identifier);
+            copy_scope(&_new->member_scope, &old->member_scope);
+            _new->member_scope.owning_enum = _new;
+            
+            // @@ Do we need to copy anything else?
+
             return _new;
         }
 
