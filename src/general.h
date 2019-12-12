613808cc21685bc9330e2ecf193c2a44561d71a6
Joshua Huelsman <joshuahuelsman@gmail.com>
2019-12-12 18:27:21 -0500

    Add a static assert.

--- src/general.h
+++ src/general.h
@@ -21,33 +21,38 @@ typedef uint8_t  u8;
 #include <string.h>
 
 #ifdef ENV64
+typedef s64 array_count_type;
 typedef s64 string_length_type;
 #else
+typedef s32 array_count_type;
 typedef s32 string_length_type;
 #endif
+
+static_assert(sizeof(array_count_type) == sizeof(void *), "sizeof(array_count_type) must match sizeof(void *).");
+// static_assert(sizeof(string_length_type) == sizeof(void *), "sizeof(string_length_type) must match sizeof(void *).");
 
 const int BYTES_TO_BITS = 8;
 
 struct String {
     char *data = nullptr;
     string_length_type length = 0;
-    
+
     String() { data = nullptr; }
-    
+
     // String(char *str) {
     //     this->data = str;
     //     this->length = strlen(str)
     // }
-    
+
     char &operator[](string_length_type index) const {
         assert(index >= 0 && index < length);
-        
+
         return data[index];
     }
-    
+
     String substring(string_length_type start, string_length_type slen) {
         assert(start < length && start+slen <= length);
-        
+
         String s;
         s.data = data + start;
         s.length = slen;
@@ -74,7 +79,7 @@ inline String to_string(const char *c_string) {
 
 inline char *to_c_string(String s) {
     auto length = s.length;
-    
+
     char *mem = (char *)malloc(length + 1);
     memcpy(mem, s.data, length);
     mem[s.length] = 0;
@@ -88,11 +93,11 @@ inline bool operator==(const String &s, const String &t) {
     if (s.data == nullptr && t.data != nullptr) return false;
     if (t.data == nullptr && s.data != nullptr) return false;
     if (s.data == nullptr && t.data == nullptr) return true;
-    
+
     for (string_length_type i = 0; i < s.length; ++i) {
         if (s[i] != t[i]) return false;
     }
-    
+
     return true;
 }
 
@@ -105,7 +110,7 @@ inline bool operator!=(const String &s, const String &t) {
 inline String copy_string(String s) {
     String out;
     out.length = s.length;
-    
+
     auto length = s.length;
     if (s.data && s.length) {
         out.data = (char *)malloc(length);
@@ -121,10 +126,10 @@ inline String basepath(String s) {
     }
     while (s.length) {
         if (s[s.length-1] == '/' || s[s.length-1] == '\\') return s;
-        
+
         s.length--;
     }
-    
+
     return s;
 }
 
@@ -142,13 +147,13 @@ inline String basename(String s) {
         if (s[length-1] == '/' || s[length-1] == '\\') {
             String out;
             out.data   = s.data + length;
-            out.length = s.length - (length + skip); 
+            out.length = s.length - (length + skip);
             return out;
         }
-        
+
         length--;
     }
-    
+
     return s;
 }
 
@@ -159,7 +164,7 @@ inline void convert_to_back_slashes(char *c) {
         if (*c == '/') {
             *c = '\\';
         }
-        
+
         ++c;
     }
 }
@@ -178,7 +183,7 @@ inline void convert_to_forward_slashes(char *c) {
         if (*c == '\\') {
             *c = '/';
         }
-        
+
         ++c;
     }
 }
@@ -187,22 +192,22 @@ inline void convert_to_forward_slashes(char *c) {
 struct Span {
     string_length_type start;
     string_length_type length;
-    
+
     Span(string_length_type start = 0, string_length_type length = 0) {
         assert(start >= 0);
         assert(length >= 0);
-        
+
         this->start = start;
         this->length = length;
     }
-    
+
     bool fits_in_string(String text) {
         return (!(text.length < start || text.length < start + length));
     }
-    
+
     void map_to_text_coordinates(String text, string_length_type *line_start, string_length_type *char_start, string_length_type *line_end, string_length_type *char_end) {
         assert(fits_in_string(text));
-        
+
         string_length_type line_count = 1;
         string_length_type char_count = 1;
         for (string_length_type i = 0; i < text.length; ++i) {
@@ -214,31 +219,31 @@ struct Span {
                 *char_end = char_count;
                 return;
             }
-            
+
             if (text[i] == '\n') {
                 line_count++;
                 char_count = 1;
                 continue;
             }
-            
+
             char_count++;
         }
     }
-    
+
     void get_surrounding_lines(String text, int num_surrounding_lines, string_length_type *new_start, string_length_type *new_end, string_length_type *return_num_lines) {
         // Get the current line(s) that this span occupies
         string_length_type line_start;
         string_length_type char_start;
         string_length_type line_end;
         string_length_type char_end;
-        
+
         map_to_text_coordinates(text, &line_start, &char_start, &line_end, &char_end);
-        
+
         string_length_type start_line = (line_start - num_surrounding_lines);
         if (start_line < 1) start_line = 1;
-        
+
         string_length_type end_line = (line_start + num_surrounding_lines) + 1; // Add one so we rollover to the start of the next line so that we capture all the text from the end line.
-        
+
         string_length_type line_count  = 1;
         string_length_type start_index = -1;
         string_length_type end_index   = -1;
@@ -249,22 +254,22 @@ struct Span {
                 end_index = i;
                 break;
             }
-            
+
             if (text[i] == '\n') {
                 line_count++;
                 continue;
             }
         }
-        
+
         if (start_index < 0) start_index = 0;
         *new_start = start_index;
-        
+
         if (end_index < 0) {
             end_line  = line_count + 1;
             end_index = text.length;
         }
         *new_end   = end_index;
-        
+
         *return_num_lines = end_line - start_line;
     }
 };
@@ -272,61 +277,55 @@ struct Span {
 struct TextSpan {
     Span span;
     String string;
-    
+
     TextSpan() {
     }
-    
+
     TextSpan(String string, Span span) {
         this->string = string;
         this->span = span;
-        
+
         assert(span.fits_in_string(string));
     }
 
     void calculate_text_coordinates(string_length_type *line_start, string_length_type *char_start, string_length_type *line_end, string_length_type *char_end) {
         span.map_to_text_coordinates(string, line_start, char_start, line_end, char_end);
     }
-    
+
     String get_text();
 };
-
-#ifdef ENV64
-typedef s64 array_count_type;
-#else
-typedef s32 array_count_type;
-#endif
 
 template<typename T>
 struct Array {
     T *data = nullptr;
     array_count_type count = 0;
     array_count_type allocated = 0;
-    
+
     const int NEW_MEM_CHUNK_ELEMENT_COUNT =  16;
-    
+
     Array(array_count_type reserve_amount = 0) {
         reserve(reserve_amount);
     }
-    
+
     ~Array() {
         reset();
     }
-    
+
     void reserve(array_count_type amount) {
         if (amount <= 0) amount = NEW_MEM_CHUNK_ELEMENT_COUNT;
         if (amount <= allocated) return;
-        
+
         T *new_mem = (T *)malloc(amount * sizeof(T));
-        
+
         if (data) {
             memcpy(new_mem, data, count * sizeof(T));
             free(data);
         }
-        
+
         data = new_mem;
         allocated = amount;
     }
-    
+
     void resize(array_count_type amount) {
         auto old_count = count;
         reserve(amount);
@@ -336,7 +335,7 @@ struct Array {
         // @TODO maybe default initalized all elements
         // that we grew by?
     }
-    
+
     void insert(array_count_type index, T element) {
         auto old_count = count;
         resize(count + 1);
@@ -347,22 +346,22 @@ struct Array {
 
     void add(T element) {
         if (count+1 >= allocated) reserve(allocated + NEW_MEM_CHUNK_ELEMENT_COUNT);
-        
+
         data[count] = element;
         count += 1;
     }
-    
+
     T unordered_remove(array_count_type index) {
         assert(index >= 0 && index < count);
         assert(count);
-        
+
         T last = pop();
         // if index is still within the valid range (index was not referencing the last item)
         // we put the last item in the slot we're removing.
         if (index < count) {
             (*this)[index] = last;
         }
-        
+
         return last;
     }
 
@@ -376,35 +375,35 @@ struct Array {
         count--;
         return item;
     }
-    
+
     T pop() {
         assert(count > 0);
         T result = data[count-1];
         count -= 1;
         return result;
     }
-    
+
     void clear() {
         count = 0;
     }
-    
+
     void reset() {
         count = 0;
         allocated = 0;
-        
+
         if (data) free(data);
         data = nullptr;
     }
-    
+
     T &operator[] (array_count_type index) {
         assert(index >= 0 && index < count);
         return data[index];
     }
-    
+
     T *begin() {
         return &data[0];
     }
-    
+
     T *end() {
         return &data[count];
     }
@@ -428,30 +427,30 @@ Tuple<A, B> MakeTuple(A a, B b) {
 #include <cstdarg>
 
 struct String_Builder {
-    
+
     const int BUCKET_ALLOC_SIZE = 4096;
-    
+
     struct Bucket {
         u8 *data = nullptr;
         array_count_type count     = 0;
         array_count_type allocated = 0;
     };
-    
+
     Array<Bucket> buckets;
-    
+
     String_Builder() {
         make_bucket(BUCKET_ALLOC_SIZE);
     }
-    
+
     ~String_Builder() {
         for (auto &bucket : buckets) {
             if (bucket.data) free(bucket.data);
             bucket.data = nullptr;
         }
-        
+
         buckets.reset();
     }
-    
+
     void make_bucket(array_count_type amount) {
         Bucket b;
         b.data = (u8 *)malloc(amount);
@@ -459,10 +458,10 @@ struct String_Builder {
         b.count = 0;
         buckets.add(b);
     }
-    
+
     void putchar(char c) {
         auto bucket = &buckets[buckets.count-1];
-        
+
         if (bucket->count < bucket->allocated) {
             bucket->data[bucket->count] = c;
             bucket->count++;
@@ -471,30 +470,30 @@ struct String_Builder {
             putchar(c);
         }
     }
-    
+
     void append(String s) {
         for (string_length_type i = 0; i < s.length; ++i) {
             putchar(s[i]);
         }
     }
-    
+
     void append(char *s) {
         String o;
         o.data = s;
         o.length = strlen(s);
         append(o);
     }
-    
+
     void print_valist(char *c_fmt, va_list vl) {
         va_list vl_copy;
         va_copy(vl_copy, vl);
-        
+
         auto bucket = &buckets[buckets.count-1];
         auto remaining = bucket->allocated - bucket->count;
         auto written = vsnprintf((char *)bucket->data + bucket->count, remaining, c_fmt, vl);
-        
+
         if (written < 0) return; // encoding error, @TODO maybe assert here?
-        
+
         if (written < remaining) {
             // success
             bucket->count += written;
@@ -502,10 +501,10 @@ struct String_Builder {
         } else {
             u8 *data = (u8 *)malloc(written + 1);
             auto final = vsnprintf((char *)data, written+1, c_fmt, vl_copy);
-            
+
             assert(final >= 0);
             assert(final < written + 1);
-            
+
             Bucket b;
             b.data = data;
             b.count = final;
@@ -513,29 +512,29 @@ struct String_Builder {
             buckets.add(b);
         }
     }
-    
+
     void print(char *c_fmt, ...) {
         va_list vl;
         va_start(vl, c_fmt);
         print_valist(c_fmt, vl);
         va_end(vl);
     }
-    
+
     String to_string() {
         array_count_type total_data = 0;
         for (Bucket &b : buckets) {
             total_data += b.count;
         }
-        
+
         char *data = (char *)malloc(total_data);
         array_count_type cursor = 0;
         for (Bucket &b : buckets) {
             memcpy(data+cursor, b.data, b.count);
             cursor += b.count;
         }
-        
+
         assert(cursor == total_data);
-        
+
         String s;
         s.data = data;
         s.length = total_data;
@@ -549,7 +548,7 @@ struct Pool {
         s64 used = 0;
         s64 allocated = 0;
     };
-    
+
     const array_count_type DEFAULT_NEW_CHUNK_SIZE = 4096 * 4;
     Array<Chunk> chunks;
 
@@ -604,13 +603,13 @@ struct ExitScope {
   private:
     ExitScope& operator =(const ExitScope&);
 };
- 
+
 class ExitScopeHelp {
   public:
     template<typename T>
         ExitScope<T> operator+(T t){ return t;}
 };
- 
+
 #if _MSC_VER
 #define defer const auto& CONCAT(defer__, __LINE__) = ExitScopeHelp() + [&]()
 #else // __GNUC__ or __clang__
