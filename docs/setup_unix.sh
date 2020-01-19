b550c64573486122f7a8c2be2600802349f51668
Joshua Huelsman <joshuahuelsman@gmail.com>
2020-01-19 15:59:09 -0500

    setup_unix.sh: Add -fPIC flags to LLVM+Clang build.

--- docs/setup_unix.sh
+++ docs/setup_unix.sh
@@ -34,6 +34,7 @@ cd build
 # Turn off as many unneeded build steps in clang+llvm as possible to speedup the build and require less resources so that this works on SBC's with a small amount of memory.
 cmake ../llvm -DCMAKE_BUILD_TYPE=$LLVM_BUILD_TYPE -DLLVM_TARGETS_TO_BUILD=$LLVM_TARGETS -DLLVM_ENABLE_DUMP=ON -DCMAKE_INSTALL_PREFIX=$JIYU_DIR/llvm \
               -DLLVM_BUILD_TOOLS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_WARNINGS=OFF \
+              -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" \
               -DLLVM_ENABLE_PROJECTS=clang -DLIBCLANG_BUILD_STATIC=ON -DLLVM_ENABLE_PIC=OFF #disbale PIC because LIBCLANG_BUILD_STATIC does not work if DLLVM_ENABLE_PIC is ON
 cmake --build . --target install -j 4
 
